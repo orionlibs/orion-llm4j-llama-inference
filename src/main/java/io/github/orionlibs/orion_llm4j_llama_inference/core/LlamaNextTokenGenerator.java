afbb5bca4ab1210e8a3807113fcd4b4fe1daedf0
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-08-30 15:49:49 +0100

    reused abstract next token generator

---
+++ src/main/java/io/github/orionlibs/orion_llm4j_llama_inference/core/LlamaNextTokenGenerator.java
@@ -0,0 +1,135 @@
+package io.github.orionlibs.orion_llm4j_llama_inference.core;
+
+import io.github.orionlibs.orion_llm4j_inference.core.Configuration;
+import io.github.orionlibs.orion_llm4j_inference.core.LLMProcessor;
+import io.github.orionlibs.orion_llm4j_inference.core.NextTokenGenerator;
+import io.github.orionlibs.orion_llm4j_inference.core.State;
+import io.github.orionlibs.orion_llm4j_inference.core.Weights;
+import io.github.orionlibs.orion_llm4j_inference.core.utils.Parallel;
+import io.github.orionlibs.orion_llm4j_llama_inference.core.tensor.SimpleFloatTensor;
+import java.nio.FloatBuffer;
+
+class LlamaNextTokenGenerator implements NextTokenGenerator
+{
+    private void rmsnorm(SimpleFloatTensor out, SimpleFloatTensor x, FloatBuffer weight, int size, float rmsNormEps)
+    {
+        // calculate sum of squares
+        float ss = x.reduce(0, size, 0f, (acc, xi) -> acc + xi * xi);
+        ss /= size;
+        ss += rmsNormEps;
+        ss = (float)(1.0 / Math.sqrt(ss));
+        // normalize and scale
+        final float finalss = ss; // for the lambda
+        out.mapWithIndexInPlace(0, size, (value, index) -> weight.get(index) * (finalss * x.getFloat(index)));
+    }
+
+
+    @Override
+    public SimpleFloatTensor generate(LLMProcessor model, State state, int token, int position)
+    {
+        SimpleState simpleState = (SimpleState)state;
+        // a few convenience variables
+        Configuration config = model.getConfiguration();
+        Weights weights = model.getWeights();
+        int dim = config.dim;
+        int headSize = config.headSize;
+        int kvDim = (config.dim * config.numberOfKeyValueHeads) / config.numberOfHeads;
+        int kvMul = config.numberOfHeads / config.numberOfKeyValueHeads; // integer multiplier of the kv sharing in multiquery
+        float sqrtHeadSize = (float)Math.sqrt(headSize);
+        // copy the token embedding into x
+        weights.token_embedding_table.copyTo(token * dim, simpleState.x, 0, dim);
+        // forward all the layers
+        for(int l = 0; l < config.numberOfLayers; l++)
+        {
+            // attention rmsnorm
+            rmsnorm(simpleState.xb, simpleState.x, weights.rms_att_weight[l], dim, config.rmsNormEps);
+            // qkv matmuls for this position
+            weights.wq[l].matmul(simpleState.xb, simpleState.q, dim, dim);
+            weights.wk[l].matmul(simpleState.xb, simpleState.k, kvDim, dim);
+            weights.wv[l].matmul(simpleState.xb, simpleState.v, kvDim, dim);
+            // RoPE relative positional encoding: complex-valued rotate q and k in each head
+            for(int i = 0; i < dim; i += 2)
+            {
+                int head_dim = i % headSize;
+                float fcr = weights.freq_cis_real.get(position * (headSize / 2) + (head_dim / 2));
+                float fci = weights.freq_cis_imag.get(position * (headSize / 2) + (head_dim / 2));
+                int rotn = i < kvDim ? 2 : 1; // how many vectors? 2 = q & k, 1 = q only
+                for(int v = 0; v < rotn; v++)
+                {
+                    SimpleFloatTensor vec = v == 0 ? simpleState.q : simpleState.k; // the vector to rotate (query or key)
+                    float v0 = vec.getFloat(i);
+                    float v1 = vec.getFloat(i + 1);
+                    vec.setFloat(i, v0 * fcr - v1 * fci);
+                    vec.setFloat(i + 1, v0 * fci + v1 * fcr);
+                }
+            }
+            // save key,value at this time step (position) to our kv cache
+            //int loff = l * config.seq_len * kvDim; // kv cache layer offset for convenience
+            simpleState.k.copyTo(0, simpleState.keyCache[l], position * kvDim, kvDim);
+            simpleState.v.copyTo(0, simpleState.valueCache[l], position * kvDim, kvDim);
+            int curLayer = l;
+            // multihead attention. iterate over all heads
+            Parallel.parallelFor(0, config.numberOfHeads, h -> {
+                // get the query vector for this head
+                // float* q = s.q + h * headSize;
+                int qOffset = h * headSize;
+                // attention scores for this head
+                // float* att = s.att + h * config.seq_len;
+                int attOffset = h * config.contextLength;
+                // iterate over all timesteps, including the current one
+                for(int t = 0; t <= position; t++)
+                {
+                    // get the key vector for this head and at this timestep
+                    // float* k = s.key_cache + loff + t * dim + h * headSize;
+                    int keyCacheOffset = /* loff + */ t * kvDim + (h / kvMul) * headSize;
+                    // calculate the attention score as the dot product of q and k
+                    float score = simpleState.q.dot(qOffset, simpleState.keyCache[curLayer], keyCacheOffset, headSize);
+                    score /= sqrtHeadSize;
+                    // save the score to the attention buffer
+                    simpleState.att.setFloat(attOffset + t, score);
+                }
+                // softmax the scores to get attention weights, from 0..position inclusively
+                simpleState.att.softmaxInPlace(attOffset, position + 1);
+                // weighted sum of the values, store back into xb
+                // float* xb = s.xb + h * headSize;
+                int xbOffset = h * headSize;
+                // memset(xb, 0, headSize * sizeof(float));
+                simpleState.xb.fillInPlace(xbOffset, headSize, 0f);
+                for(int t = 0; t <= position; t++)
+                {
+                    // get the value vector for this head and at this timestep
+                    // float* v = s.value_cache + loff + t * dim + h * headSize;
+                    int vOffset = /* loff + */ t * kvDim + (h / kvMul) * headSize;
+                    // get the attention weight for this timestep
+                    float a = simpleState.att.getFloat(attOffset + t);
+                    // accumulate the weighted value into xb
+                    simpleState.xb.saxpyInPlace(xbOffset, simpleState.valueCache[curLayer], vOffset, headSize, a);
+                }
+            });
+            // final matmul to get the output of the attention
+            weights.wo[l].matmul(simpleState.xb, simpleState.xb2, dim, dim);
+            // residual connection back into x
+            simpleState.x.addInPlace(simpleState.xb2);
+            // ffn rmsnorm
+            rmsnorm(simpleState.xb, simpleState.x, weights.rms_ffn_weight[l], dim, config.rmsNormEps);
+            // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
+            // first calculate self.w1(x) and self.w3(x)
+            weights.w1[l].matmul(simpleState.xb, simpleState.hb, config.hiddenDim, dim);
+            weights.w3[l].matmul(simpleState.xb, simpleState.hb2, config.hiddenDim, dim);
+            // SwiGLU non-linearity
+            // silu(x)=x*σ(x), where σ(x) is the logistic sigmoid
+            simpleState.hb.mapInPlace(value -> value / (float)(1.0 + Math.exp(-value)));
+            // elementwise multiply with w3(x)
+            simpleState.hb.multiplyInPlace(simpleState.hb2);
+            // final matmul to get the output of the ffn
+            weights.w2[l].matmul(simpleState.hb, simpleState.xb, dim, config.hiddenDim);
+            // residual connection
+            simpleState.x.addInPlace(simpleState.xb);
+        }
+        // final rmsnorm
+        rmsnorm(simpleState.x, simpleState.x, weights.rms_final_weight, dim, config.rmsNormEps);
+        // classifier into logits
+        weights.wcls.matmul(simpleState.x, simpleState.logits, config.vocabularySize, dim);
+        return simpleState.logits;
+    }
+}
--- src/main/java/io/github/orionlibs/orion_llm4j_llama_inference/core/SimpleLLMProcessor.java
+++ src/main/java/io/github/orionlibs/orion_llm4j_llama_inference/core/SimpleLLMProcessor.java
@@ -6,9 +6,6 @@ import io.github.orionlibs.orion_llm4j_inference.core.Response;
 import io.github.orionlibs.orion_llm4j_inference.core.State;
 import io.github.orionlibs.orion_llm4j_inference.core.Weights;
 import io.github.orionlibs.orion_llm4j_inference.core.sampler.Sampler;
-import io.github.orionlibs.orion_llm4j_inference.core.utils.Parallel;
-import io.github.orionlibs.orion_llm4j_llama_inference.core.tensor.SimpleFloatTensor;
-import java.nio.FloatBuffer;
 import java.util.List;
 import java.util.Set;
 import java.util.function.IntConsumer;
@@ -20,128 +17,7 @@ public abstract class SimpleLLMProcessor extends LLMProcessor
 
     public SimpleLLMProcessor(Configuration configuration, SimpleTokenizer tokenizer, Weights weights)
     {
-        super(configuration, tokenizer, weights);
-    }
-
-
-    static void rmsnorm(SimpleFloatTensor out, SimpleFloatTensor x, FloatBuffer weight, int size, float rmsNormEps)
-    {
-        // calculate sum of squares
-        float ss = x.reduce(0, size, 0f, (acc, xi) -> acc + xi * xi);
-        ss /= size;
-        ss += rmsNormEps;
-        ss = (float)(1.0 / Math.sqrt(ss));
-        // normalize and scale
-        final float finalss = ss; // for the lambda
-        out.mapWithIndexInPlace(0, size, (value, index) -> weight.get(index) * (finalss * x.getFloat(index)));
-    }
-
-
-    static SimpleFloatTensor forward(LLMProcessor model, SimpleState state, int token, int position)
-    {
-        // a few convenience variables
-        Configuration config = model.getConfiguration();
-        Weights weights = model.getWeights();
-        int dim = config.dim;
-        int headSize = config.headSize;
-        int kvDim = (config.dim * config.numberOfKeyValueHeads) / config.numberOfHeads;
-        int kvMul = config.numberOfHeads / config.numberOfKeyValueHeads; // integer multiplier of the kv sharing in multiquery
-        float sqrtHeadSize = (float)Math.sqrt(headSize);
-        // copy the token embedding into x
-        weights.token_embedding_table.copyTo(token * dim, state.x, 0, dim);
-        // forward all the layers
-        for(int l = 0; l < config.numberOfLayers; l++)
-        {
-            // attention rmsnorm
-            rmsnorm(state.xb, state.x, weights.rms_att_weight[l], dim, config.rmsNormEps);
-            // qkv matmuls for this position
-            weights.wq[l].matmul(state.xb, state.q, dim, dim);
-            weights.wk[l].matmul(state.xb, state.k, kvDim, dim);
-            weights.wv[l].matmul(state.xb, state.v, kvDim, dim);
-            // RoPE relative positional encoding: complex-valued rotate q and k in each head
-            for(int i = 0; i < dim; i += 2)
-            {
-                int head_dim = i % headSize;
-                float fcr = weights.freq_cis_real.get(position * (headSize / 2) + (head_dim / 2));
-                float fci = weights.freq_cis_imag.get(position * (headSize / 2) + (head_dim / 2));
-                int rotn = i < kvDim ? 2 : 1; // how many vectors? 2 = q & k, 1 = q only
-                for(int v = 0; v < rotn; v++)
-                {
-                    SimpleFloatTensor vec = v == 0 ? state.q : state.k; // the vector to rotate (query or key)
-                    float v0 = vec.getFloat(i);
-                    float v1 = vec.getFloat(i + 1);
-                    vec.setFloat(i, v0 * fcr - v1 * fci);
-                    vec.setFloat(i + 1, v0 * fci + v1 * fcr);
-                }
-            }
-            // save key,value at this time step (position) to our kv cache
-            //int loff = l * config.seq_len * kvDim; // kv cache layer offset for convenience
-            state.k.copyTo(0, state.keyCache[l], position * kvDim, kvDim);
-            state.v.copyTo(0, state.valueCache[l], position * kvDim, kvDim);
-            int curLayer = l;
-            // multihead attention. iterate over all heads
-            Parallel.parallelFor(0, config.numberOfHeads, h -> {
-                // get the query vector for this head
-                // float* q = s.q + h * headSize;
-                int qOffset = h * headSize;
-                // attention scores for this head
-                // float* att = s.att + h * config.seq_len;
-                int attOffset = h * config.contextLength;
-                // iterate over all timesteps, including the current one
-                for(int t = 0; t <= position; t++)
-                {
-                    // get the key vector for this head and at this timestep
-                    // float* k = s.key_cache + loff + t * dim + h * headSize;
-                    int keyCacheOffset = /* loff + */ t * kvDim + (h / kvMul) * headSize;
-                    // calculate the attention score as the dot product of q and k
-                    float score = state.q.dot(qOffset, state.keyCache[curLayer], keyCacheOffset, headSize);
-                    score /= sqrtHeadSize;
-                    // save the score to the attention buffer
-                    state.att.setFloat(attOffset + t, score);
-                }
-                // softmax the scores to get attention weights, from 0..position inclusively
-                state.att.softmaxInPlace(attOffset, position + 1);
-                // weighted sum of the values, store back into xb
-                // float* xb = s.xb + h * headSize;
-                int xbOffset = h * headSize;
-                // memset(xb, 0, headSize * sizeof(float));
-                state.xb.fillInPlace(xbOffset, headSize, 0f);
-                for(int t = 0; t <= position; t++)
-                {
-                    // get the value vector for this head and at this timestep
-                    // float* v = s.value_cache + loff + t * dim + h * headSize;
-                    int vOffset = /* loff + */ t * kvDim + (h / kvMul) * headSize;
-                    // get the attention weight for this timestep
-                    float a = state.att.getFloat(attOffset + t);
-                    // accumulate the weighted value into xb
-                    state.xb.saxpyInPlace(xbOffset, state.valueCache[curLayer], vOffset, headSize, a);
-                }
-            });
-            // final matmul to get the output of the attention
-            weights.wo[l].matmul(state.xb, state.xb2, dim, dim);
-            // residual connection back into x
-            state.x.addInPlace(state.xb2);
-            // ffn rmsnorm
-            rmsnorm(state.xb, state.x, weights.rms_ffn_weight[l], dim, config.rmsNormEps);
-            // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
-            // first calculate self.w1(x) and self.w3(x)
-            weights.w1[l].matmul(state.xb, state.hb, config.hiddenDim, dim);
-            weights.w3[l].matmul(state.xb, state.hb2, config.hiddenDim, dim);
-            // SwiGLU non-linearity
-            // silu(x)=x*σ(x), where σ(x) is the logistic sigmoid
-            state.hb.mapInPlace(value -> value / (float)(1.0 + Math.exp(-value)));
-            // elementwise multiply with w3(x)
-            state.hb.multiplyInPlace(state.hb2);
-            // final matmul to get the output of the ffn
-            weights.w2[l].matmul(state.hb, state.xb, dim, config.hiddenDim);
-            // residual connection
-            state.x.addInPlace(state.xb);
-        }
-        // final rmsnorm
-        rmsnorm(state.x, state.x, weights.rms_final_weight, dim, config.rmsNormEps);
-        // classifier into logits
-        weights.wcls.matmul(state.x, state.logits, config.vocabularySize, dim);
-        return state.logits;
+        super(configuration, tokenizer, weights, new LlamaNextTokenGenerator());
     }
 
 
@@ -186,7 +62,7 @@ public abstract class SimpleLLMProcessor extends LLMProcessor
         int promptIndex = 0;
         for(int position = startPosition; position < maxTokens; position++)
         {
-            forward(model, simpleState, token, position);
+            nextTokenGenerator.generate(model, simpleState, token, position);
             //this is the prompt itself
             if(promptIndex < promptTokens.size())
             {
